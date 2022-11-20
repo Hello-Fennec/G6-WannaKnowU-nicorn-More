40724f40c794c614676571d1c11bb2c2dafa9a74
tanedgame <gamegamethanet@gmail.com>
2022-11-21 00:28:46 +0700

    update nav

--- wannaknowunicornmore/src/App.js
+++ wannaknowunicornmore/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import ResponsiveAppBar from './components/Navbar';
+import BoxSx from './components/Box';
 
 function App() {
   return (
-    <ResponsiveAppBar />
+    <div className='UnicornApp'>
+        <ResponsiveAppBar />
+        <BoxSx />
+    </div>
   );
 }
 
