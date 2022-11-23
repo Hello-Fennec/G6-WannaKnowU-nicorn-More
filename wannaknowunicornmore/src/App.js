b48f603629739901c84298085334e902931a5841
tanedgame <gamegamethanet@gmail.com>
2022-11-23 21:30:22 +0700

    update

--- wannaknowunicornmore/src/App.js
+++ wannaknowunicornmore/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import ResponsiveAppBar from './components/Navbar';
+import NavBar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className='UnicornApp'>
-        <ResponsiveAppBar />
+        <NavBar />
         <Home />
     </div>
   );
